e8cb5b106398d7196cec7725c7ea99c349faa7a0
Abdou Samath <117266746+AbdouSamathDiouf@users.noreply.github.com>
2024-01-17 16:27:13 +0100

    Ajout des composants suivants: Navbar.vue, ResourceCard.vue, ResourceDetailCard.vue, ResourceForm.vue et ResourceList.vue.

--- Gestion-de-bibliotheque/src/router/index.js
+++ Gestion-de-bibliotheque/src/router/index.js
@@ -1,25 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Vue from 'vue';
-import Router from 'vue-router';
-import Accueil from '@/views/HomeApp.vue';
-import Thematique from '@/views/ThemeApp.vue';
+//import Vue from 'vue';
+//import Router from 'vue-router';
+import HomeApp from '@/views/HomeApp.vue';
+import ThemeApp from '@/views/ThemeApp.vue';
 import ResourceDetail from '@/views/ResourceDetail.vue';
 import AddResource from '@/views/AddResource.vue';
 import NotFound from '@/views/NotFound.vue';
 
-Vue.use(Router);
+//Vue.use(Router);
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
      {
       path: '/',
       name: 'Home',
-      component: Accueil,
+      component: HomeApp,
     },
     {
       path: '/theme/:themeId',
       name: 'Theme',
-      component: Thematique,
+      component: ThemeApp,
     },
     {
       path: '/resource/:resourceId',
